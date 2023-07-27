9c09595db3fedc4782137ea50d0deef1613e8646
Tri Than <thantrongtri3@gmail.com>
2023-07-27 17:28:12 +0700

    authentication middleware

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+  // get token from header
+  const token = req.header('x-auth-token');
+
+  // no token, deny access
+  if (! token) {
+    return res.status(401).json({ msg: 'No token provided, access denied' });
+  }
+
+  // verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Unauthorized access' });
+  }
+}
