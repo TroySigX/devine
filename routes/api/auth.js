cfcd33912c3bc93e8ca73508bf9cfd1debe60b86
Tri Than <thantrongtri3@gmail.com>
2023-08-01 11:37:34 +0700

    backend: do not hide email when returning user to api

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -13,7 +13,7 @@ const User = require('../../models/User');
 // @access  Public
 router.get('/', auth, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select('-password -email');
+    const user = await User.findById(req.user.id).select('-password');
     res.json(user);
   } catch (err) {
     console.error(err.message);
