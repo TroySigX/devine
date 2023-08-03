791d010e5c94696383f1bc070633d925ece36872
Tri Than <thantrongtri3@gmail.com>
2023-08-03 15:54:43 +0700

    frontend: dashboard

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,42 @@
+import React, { useEffect, Fragment } from 'react';
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
+import Spinner from '../layout/Spinner';
+
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
+  return loading && profile === null ? (
+    <Spinner />
+  ) : profile !== null ? (
+    <section className='container'>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome {user && user.name}
+      </p>
+    </section>
+  ) : (
+    <Navigate to='/create-profile' />
+  );
+};
+
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
