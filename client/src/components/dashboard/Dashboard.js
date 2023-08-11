25b5b31f0952ec21b69f4e957be356589a972b63
Tri Than <thantrongtri3@gmail.com>
2023-08-11 16:04:14 +0700

    dashboard interface

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,30 +1,53 @@
 import React, { useEffect, Fragment } from 'react';
-import { Navigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profile';
-import Spinner from '../layout/Spinner';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
+import { DashboardActions } from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
+import { Link } from 'react-router-dom';
 
 const Dashboard = ({
   getCurrentProfile,
   auth: { user },
-  profile: { profile, loading },
+  profile: { profile },
+  deleteAccount,
 }) => {
   useEffect(() => {
     getCurrentProfile();
-  }, []);
+  }, [getCurrentProfile]);
 
-  return loading && profile === null ? (
-    <Spinner />
-  ) : profile !== null ? (
+  return (
     <section className='container'>
       <h1 className='large text-primary'>Dashboard</h1>
       <p className='lead'>
         <i className='fas fa-user' /> Welcome {user && user.name}
       </p>
+      {profile ? (
+        <Fragment>
+          <DashboardActions />
+          {profile.experience.length > 0 && (
+            <Experience experience={profile.experience} />
+          )}
+          {profile.education.length > 0 && (
+            <Education education={profile.education} />
+          )}
+
+          <div className='my-2'>
+            <button className='btn btn-danger' onClick={deleteAccount}>
+              <i className='fas fa-user-minus' /> Delete My Account
+            </button>
+          </div>
+        </Fragment>
+      ) : (
+        <>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            Create Profile
+          </Link>
+        </>
+      )}
     </section>
-  ) : (
-    <Navigate to='/create-profile' />
   );
 };
 
@@ -32,6 +55,7 @@ Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
@@ -39,4 +63,6 @@ const mapStateToProps = (state) => ({
   profile: state.profile,
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
+  Dashboard
+);
