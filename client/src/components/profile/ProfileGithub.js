24f03e69364d9cbd8492686d6062903bbbc4007b
Tri Than <thantrongtri3@gmail.com>
2023-08-15 22:44:13 +0700

    fix(github display): only show the "Github Repos" title when at least 1 repo exists

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getGithubRepos } from '../../actions/profile';
@@ -10,38 +10,44 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
 
   return (
     <div className='profile-github'>
-      <h2 className='text-primary my-1'>Github Repos</h2>
-      {repos
-        .toSorted(
-          (repo1, repo2) => repo2.stargazers_count - repo1.stargazers_count
-        )
-        .map((repo) => (
-          <div key={repo.id} className='repo bg-white p-1 my-1'>
-            <div>
-              <h4>
-                <a
-                  href={repo.html_url}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  {repo.name}
-                </a>
-              </h4>
-              <p>{repo.description}</p>
-            </div>
-            <div>
-              <ul>
-                <li className='badge badge-primary'>
-                  Stars: {repo.stargazers_count}
-                </li>
-                <li className='badge badge-dark'>
-                  Watchers: {repo.watchers_count}
-                </li>
-                <li className='badge badge-light'>Forks: {repo.forks_count}</li>
-              </ul>
-            </div>
-          </div>
-        ))}
+      {repos.length > 0 && (
+        <Fragment>
+          <h2 className='text-primary my-1'>Github Repos</h2>
+          {repos
+            .toSorted(
+              (repo1, repo2) => repo2.stargazers_count - repo1.stargazers_count
+            )
+            .map((repo) => (
+              <div key={repo.id} className='repo bg-white p-1 my-1'>
+                <div>
+                  <h4>
+                    <a
+                      href={repo.html_url}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                    >
+                      {repo.name}
+                    </a>
+                  </h4>
+                  <p>{repo.description}</p>
+                </div>
+                <div>
+                  <ul>
+                    <li className='badge badge-primary'>
+                      Stars: {repo.stargazers_count}
+                    </li>
+                    <li className='badge badge-dark'>
+                      Watchers: {repo.watchers_count}
+                    </li>
+                    <li className='badge badge-light'>
+                      Forks: {repo.forks_count}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            ))}
+        </Fragment>
+      )}
     </div>
   );
 };
