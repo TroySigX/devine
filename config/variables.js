47b5b77fae7c11c915ae33d67180f86ace30fc8e
Tri Than <thantrongtri3@gmail.com>
2023-08-15 21:30:51 +0700

    config: check null variables

--- config/variables.js
+++ config/variables.js
@@ -1,20 +1,22 @@
+const conf = require('config');
+
 // github token
 const githubToken =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === 'production' && process.env.githubToken
     ? process.env.githubToken
-    : config.get('githubToken');
+    : conf.get('githubToken');
 
 // jwt secret
 const jwtSecret =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === 'production' && process.env.jwtSecret
     ? process.env.jwtSecret
-    : config.get('jwtSecret');
+    : conf.get('jwtSecret');
 
 // mongodb URI
 const mongoURI =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === 'production' && process.env.mongoURI
     ? process.env.mongoURI
-    : config.get('mongoURI');
+    : conf.get('mongoURI');
 
 const vars = { githubToken, jwtSecret, mongoURI };
 
