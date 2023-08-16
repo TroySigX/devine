32a4f6e9842691c289b03424076689dfb735a4f5
Tri Than <thantrongtri3@gmail.com>
2023-08-16 22:35:51 +0700

    feat(dashboard): add spinner indicating loading

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import { DashboardActions } from './DashboardActions';
 import Experience from './Experience';
 import Education from './Education';
 import { Link } from 'react-router-dom';
+import Spinner from '../layout/Spinner';
 
 const Dashboard = ({
   getCurrentProfile,
@@ -18,36 +19,42 @@ const Dashboard = ({
   }, [getCurrentProfile]);
 
   return (
-    <section className='container'>
-      <h1 className='large text-primary'>Dashboard</h1>
-      <p className='lead'>
-        <i className='fas fa-user' /> Welcome {user && user.name}
-      </p>
-      {profile && profile.created ? (
-        <Fragment>
-          <DashboardActions />
-          {profile.experience.length > 0 && (
-            <Experience experience={profile.experience} />
-          )}
-          {profile.education.length > 0 && (
-            <Education education={profile.education} />
-          )}
-
-          <div className='my-2'>
-            <button className='btn btn-danger' onClick={deleteAccount}>
-              <i className='fas fa-user-minus' /> Delete My Account
-            </button>
-          </div>
-        </Fragment>
+    <Fragment>
+      {profile === null ? (
+        <Spinner />
       ) : (
-        <>
-          <p>You have not yet setup a profile, please add some info</p>
-          <Link to='/create-profile' className='btn btn-primary my-1'>
-            Create Profile
-          </Link>
-        </>
+        <section className='container'>
+          <h1 className='large text-primary'>Dashboard</h1>
+          <p className='lead'>
+            <i className='fas fa-user' /> Welcome {user && user.name}
+          </p>
+          {profile.created ? (
+            <Fragment>
+              <DashboardActions />
+              {profile.experience.length > 0 && (
+                <Experience experience={profile.experience} />
+              )}
+              {profile.education.length > 0 && (
+                <Education education={profile.education} />
+              )}
+
+              <div className='my-2'>
+                <button className='btn btn-danger' onClick={deleteAccount}>
+                  <i className='fas fa-user-minus' /> Delete My Account
+                </button>
+              </div>
+            </Fragment>
+          ) : (
+            <>
+              <p>You have not yet setup a profile, please add some info</p>
+              <Link to='/create-profile' className='btn btn-primary my-1'>
+                Create Profile
+              </Link>
+            </>
+          )}
+        </section>
       )}
-    </section>
+    </Fragment>
   );
 };
 
